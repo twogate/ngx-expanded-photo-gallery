a6021d8a64ba420824c7a3f8000db5c2b88075f2
John-bardera <johnbardera@gmail.com>
2021-05-12 18:22:10 +0900

    refactor initUniqueButtonEl

--- projects/core/src/lib/services/expanded-options.service.ts
+++ projects/core/src/lib/services/expanded-options.service.ts
@@ -29,32 +29,34 @@ export class ExpandedOptionsService {
   }
 
   private initUniqueButtonEl(hasUniqueButtonEl: boolean, uniqueButtonOptions: UniqueButtonOption[])  {
-    if (hasUniqueButtonEl && uniqueButtonOptions.length) {
-      const uniqueButtonElHTML: Element = this.lightbox.getElementsByClassName('pswp__button--unique')[0];
-      let classes: string[] = [];
-      Array.from((new Array(uniqueButtonElHTML.children.length)).keys())
-        .forEach((i: number) =>
-          uniqueButtonElHTML.children[i].classList.forEach((className: string) => classes.push(className)));
-      classes = new Array(...(new Set(classes)));
-
-      uniqueButtonOptions.forEach((option) => {
-        if (classes.includes(`pswp__button--${this.camel2Kebab(option.eventName)}`) || (!option?.text && !option?.image)) {
-          return;
-        }
-        const button = this.renderer.createElement('button');
-        this.renderer.addClass(button, 'pswp__button');
-        this.renderer.addClass(button, `pswp__button--${this.camel2Kebab(option.eventName)}`);
-        if (option?.image) {
-          this.renderer.setStyle(button, 'background-image', `url(${option.image}`);
-        } else if (option?.text) {
-          this.renderer.addClass(button, 'pswp__button--no-image');
-          this.renderer.appendChild(button, this.renderer.createText(option.text));
-        }
-        this.renderer.setAttribute(button, 'title', option.title);
-        this.renderer.listen(button, 'click', option?.eventFn || (() => {}));
-        this.renderer.appendChild(uniqueButtonElHTML, button);
-      });
+    const uniqueButtonElHTML: Element = this.lightbox.getElementsByClassName('pswp__button--unique')[0];
+    if (!hasUniqueButtonEl || !uniqueButtonOptions.length || !uniqueButtonElHTML) {
+      return;
     }
+
+    let classes: string[] = [];
+    Array.from((new Array(uniqueButtonElHTML.children.length)).keys())
+      .forEach((i: number) =>
+        uniqueButtonElHTML.children[i].classList.forEach((className: string) => classes.push(className)));
+    classes = new Array(...(new Set(classes)));
+
+    uniqueButtonOptions.forEach((option) => {
+      if (classes.includes(`pswp__button--${this.camel2Kebab(option.eventName)}`) || (!option?.text && !option?.image)) {
+        return;
+      }
+      const button = this.renderer.createElement('button');
+      this.renderer.addClass(button, 'pswp__button');
+      this.renderer.addClass(button, `pswp__button--${this.camel2Kebab(option.eventName)}`);
+      if (option?.image) {
+        this.renderer.setStyle(button, 'background-image', `url(${option.image}`);
+      } else if (option?.text) {
+        this.renderer.addClass(button, 'pswp__button--no-image');
+        this.renderer.appendChild(button, this.renderer.createText(option.text));
+      }
+      this.renderer.setAttribute(button, 'title', option.title);
+      this.renderer.listen(button, 'click', option?.eventFn || (() => {}));
+      this.renderer.appendChild(uniqueButtonElHTML, button);
+    });
   }
   private camel2Kebab(camel: string): string {
     return camel
